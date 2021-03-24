f4b6ac2cfc10253d306f7d6cb0bb1f0a1a354f70
AriaTsai <JOUAN@boltun.com.tw>
2021-03-24 15:35:35 +0800

    update form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import {
+  FormBuilder, FormGroup, Validators, ValidatorFn, FormControl, FormArray, AbstractControl
+} from '@angular/forms';
+
+import { trim } from 'lodash';
 
 //service
 import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-cart',
@@ -13,38 +17,74 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
   title: string = '';
   checkoutForm: FormGroup;
-
   get cartList() {
     return this.CartService.getItems()
+  }
+
+  get contactionAry() {
+    return this.checkoutForm.get('contaction') as FormArray
   }
 
   constructor(
     private CartService: CartService,
     private FormBuilder: FormBuilder,
-    private router: Router
   ) {
-    this.checkoutForm = this.FormBuilder.group({
-      name: null,
-      address: null
-    })
-  }
 
+    this.checkoutForm = new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.maxLength(5), this.forbiddenSpaceValidator()]),
+      address: new FormControl(null, [Validators.required, Validators.maxLength(50), this.forbiddenSpaceValidator()]),
+      contaction: new FormArray([
+        new FormGroup({
+          type: new FormControl('Home', [Validators.required]),
+          numbers: new FormControl(null, [Validators.required, Validators.maxLength(10), this.forbiddenSpaceValidator()]),
+        })
+      ])
+    })
+
+  }
 
   ngOnInit(): void {
   }
 
-  onSubmit(customerData) {
-    this.CartService.clearCart();
-    this.checkoutForm.reset();
-    console.warn(customerData)
-    this.router.navigate(['/'])
+  forbiddenSpaceValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      if (control.dirty) {
+        const forbidden = !trim(control.value)
+        return forbidden ? { space: true } : null
+      }
+    };
   }
-  removeItem(idx) {
-    this.CartService.removeItem(idx)
-    window.alert('Delete completed!')
+
+  removeItem(idx: number) {
+    if (confirm('確認刪除?')) {
+      this.CartService.removeItem(idx)
+    }
   }
-  clearCart() {
-    this.CartService.clearCart()
-    window.alert('All clear!')
+
+  addNumbers() {
+    this.contactionAry.push(
+      new FormGroup({
+        type: new FormControl('Home', [Validators.required]),
+        numbers: new FormControl(null, [Validators.required, Validators.maxLength(10), this.forbiddenSpaceValidator()]),
+      })
+    )
   }
+
+  removeNumbers(idx: number) {
+    if (this.contactionAry.controls.length > 1) {
+      this.contactionAry.removeAt(idx)
+    }
+  }
+
+  onSubmit(checkoutForm: FormGroup) {
+    if (!checkoutForm.valid) {
+      console.warn('submit failure')
+    } else {
+      console.log(checkoutForm.value)
+      //this.CartService.clearCart();
+      //this.checkoutForm.reset();
+      //this.router.navigate(['/'])
+    }
+  }
+
 }
